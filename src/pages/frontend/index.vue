1bfe222458c82ae8c94d024ece9d03f8b7ad2310
liushijie <3116336794@qq.com>
2024-06-15 10:44:10 +0800

    vue 初始化

---
+++ src/pages/frontend/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex justify-center space-x-10 bg-slate-400">
+        <div class="text-yellow-400">黄色</div>
+
+        <div class="text-red-400">红色</div>
+
+        <div class="text-blue-400">蓝色</div>
+    </div>
+
+
+
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createWebHashHistory, createRouter } from "vue-router";
+import Index from "@/pages/frontend/index.vue";
+
+
+
+// 统一在这里声明所有路由
+const routes = [
+    {
+        path: '/', // 路由地址
+        component: Index, // 对应组件
+        meta: { // meta 信息
+            title: 'Weblog 首页' // 页面标题
+        }
+    }
+]
+
+
+// 创建路由
+const router = createRouter({
+    // 指定路由的历史管理方式，hash 模式指的是 URL 的路径是通过 hash 符号（#）进行标识
+    history: createWebHashHistory(),
+    // routes: routes 的缩写
+    routes, 
+})
+
+export default router;
